3781f9406742256ef8d7e933e0d4f5f158b374f2
Vito Jihad Abdillah <jihadvito@gmail.com>
2025-09-16 20:14:31 +0700

    QuerySQL

--- execution_test.go
+++ execution_test.go
@@ -21,3 +21,26 @@ func TestExecute(t *testing.T) {
 
 	fmt.Println("Success insert new customer")
 }
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	script := "SELECT * FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		var id, name string
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("id   :", id)
+		fmt.Println("name :", name)
+	}
+	defer rows.Close()
+}
