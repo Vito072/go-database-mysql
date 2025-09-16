fee44fb510ffbe92abc2f709caf545e92528b53e
Vito Jihad Abdillah <jihadvito@gmail.com>
2025-09-16 16:58:04 +0700

    Open Connection

---
+++ database_test.go
@@ -0,0 +1,21 @@
+package go_database_mysql
+
+import (
+	"database/sql"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func TestDatabase(t *testing.T) {
+
+}
+
+func TestOpenConnection(t *testing.T) {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/go-database")
+	if err != nil {
+		panic(err)
+	}
+
+	db.Close()
+}
