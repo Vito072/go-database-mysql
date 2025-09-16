480c16856f2f3e37a84edce10345eb816caf2f66
Vito Jihad Abdillah <jihadvito@gmail.com>
2025-09-16 19:49:48 +0700

    Get Connection

---
+++ database.go
@@ -0,0 +1,19 @@
+package go_database_mysql
+
+import (
+	"database/sql"
+	"time"
+)
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/go-database")
+	if err != nil {
+		panic(err)
+	}
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(1 * time.Hour)
+
+	return db
+}
